118a504ed0b3ebfde1c371b0baf4de2c70068638
wiley68 <ilko.iv@gmail.com>
2019-08-16 16:39:38 +0300

    images sliders

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,35 +28,35 @@
 										<ul>
 											<!-- - - - - - - - - - - - - - Slide 1 - - - - - - - - - - - - - - - - -->
 											<li data-transition="papercut" data-slotamount="7">
-												<img src="/images/home_slide_1.jpg" alt="">
-												<div class="caption sfl stl layer_1" data-x="left" data-hoffset="60" data-y="90" data-easing="easeOutBack" data-speed="600" data-start="900">Best Quality</div>
-												<div class="caption sfl stl layer_2" data-x="left" data-y="138" data-hoffset="60" data-easing="easeOutBack" data-speed="600" data-start="1000">Medications</div>
-												<div class="caption sfl stl layer_3" data-x="left" data-y="190" data-hoffset="60" data-easing="easeOutBack" data-speed="600" data-start="1100">at Low Prices</div>
+												<img src="/images/slide_comp.jpg" alt="Авалон магазин компютри">
+												<div class="caption sfl stl layer_1" data-x="left" data-hoffset="60" data-y="90" data-easing="easeOutBack" data-speed="600" data-start="900">Най-добро качество</div>
+												<div class="caption sfl stl layer_2" data-x="left" data-y="138" data-hoffset="60" data-easing="easeOutBack" data-speed="600" data-start="1000">Игрални компютри</div>
+												<div class="caption sfl stl layer_3" data-x="left" data-y="190" data-hoffset="60" data-easing="easeOutBack" data-speed="600" data-start="1100">на отлична цена</div>
 												<div class="caption sfb stb" data-x="left" data-y="245" data-hoffset="60" data-easing="easeOutBack" data-speed="700" data-start="1100">
-													<a href="#" class="button_blue big_btn">Shop Now!</a>
+													<a href="{{ route('products') }}" class="button_blue big_btn">Към нашия магазин!</a>
 												</div>
 											</li>
 											<!-- - - - - - - - - - - - - - End of Slide 1 - - - - - - - - - - - - - - - - -->
 											<!-- - - - - - - - - - - - - - Slide 2 - - - - - - - - - - - - - - - - -->
 											<li data-transition="papercut" data-slotamount="7" class="align_center">
 
-												<img src="/images/home_slide_5.jpg" alt="">
-												<div class="caption sfl stl layer_5" data-x="center" data-y="77" data-easing="easeOutBack" data-speed="600" data-start="900">Have A Question?</div>
-												<div class="caption sfl stl layer_6" data-x="center" data-y="135" data-easing="easeOutBack" data-speed="600" data-start="1050"><small>Our</small> Pharmacists<br><small>Are</small> Ready <small>to</small> Help You!</div>
+												<img src="/images/slide_software.jpg" alt="">
+												<div class="caption sfl stl layer_5" data-x="center" data-y="77" data-easing="easeOutBack" data-speed="600" data-start="900">Разработка на софтуер</div>
+												<div class="caption sfl stl layer_6" data-x="center" data-y="135" data-easing="easeOutBack" data-speed="600" data-start="1050">Програми<br><small> и</small> Модули <small>по</small> Поръчка!</div>
 												<div class="caption sfb stb" data-x="center" data-y="260" data-easing="easeOutBack" data-speed="700" data-start="1150">
-													<a href="#" class="button_blue big_btn">Contact Us Now!</a>
+													<a href="{{ route('contact') }}" class="button_blue big_btn">Свържете се с нас!</a>
 												</div>
 											</li>
 											<!-- - - - - - - - - - - - - - End of Slide 2 - - - - - - - - - - - - - - - - -->
 											<!-- - - - - - - - - - - - - - Slide 3 - - - - - - - - - - - - - - - - -->
 											<li data-transition="papercut" data-slotamount="7">
 
-												<img src="/images/home_slide_6.jpg" alt="">
-												<div class="caption sfl stl layer_8" data-x="right" data-y="73" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="900">Get 10% Off</div>
-												<div class="caption sfl stl layer_9" data-x="right" data-y="122" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="1000">For Reorders</div>
-												<div class="caption sfl stl layer_10" data-x="right" data-y="178" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="1100">Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus.</div>
+												<img src="/images/slide_ecommerce.jpg" alt="">
+												<div class="caption sfl stl layer_8" data-x="right" data-y="73" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="900">Модули за</div>
+												<div class="caption sfl stl layer_9" data-x="right" data-y="122" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="1000">e-commerce платформи</div>
+												<div class="caption sfl stl layer_10" data-x="right" data-y="178" data-hoffset="-60" data-easing="easeOutBack" data-speed="600" data-start="1100">Magento, OpenCart, PrestaShop, WooCommerce, VirtueMart ...</div>
 												<div class="caption sfb stb" data-x="right" data-hoffset="-60" data-y="262" data-easing="easeOutBack" data-speed="700" data-start="1150">
-													<a href="#" class="button_blue big_btn">Read More</a>
+													<a href="/products.html?category_id%5B0%5D=36" class="button_blue big_btn">Виж повече</a>
 												</div>
 											</li>
 											<!-- - - - - - - - - - - - - - End of Slide 3 - - - - - - - - - - - - - - - - -->
@@ -69,8 +69,8 @@
 							<!-- - - - - - - - - - - - - - End of main slider - - - - - - - - - - - - - - - - -->
 							<!-- - - - - - - - - - - - - - Banners - - - - - - - - - - - - - - - - -->
 							<div class="col-sm-3">
-								<a href="#" class="banner">
-									<img src="/images/banner_img_10.png" alt="">
+								<a href="#" class="banner" title="Покупка на стоки на изплащане">
+									<img src="/images/banner_lizing.jpg" alt="Стоки на изплащане" style="border: 1px solid #A9A9A9;">
 								</a>
 								<a href="{{ route('product', ['id' => 1]) }}" class="banner">
 									<img src="/images/cmr.png" alt="">
