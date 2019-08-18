6fa901cd6c139f06f615f2cdd57d62cb8ccef645
Ilko Ivanov <ilko.iv@gmail.com>
2019-08-18 16:02:07 +0300

    desktop programs

--- resources/views/desktop/maxtrade_ctm.blade.php
+++ resources/views/desktop/maxtrade_ctm.blade.php
@@ -1,18 +1,32 @@
+<?php use App\Product; ?>
 @extends('layouts.design')
 @section('content')
 
 <div class="page_wrapper">
     <div class="container">
-        <div class="section_offset">
-            <div class="row">
-                <h2>Програма за управление на дейността на Служба по трудова медицина</h2>
-                <h4>I. КЪДЕ МОЖЕМ ДА ИЗПОЛЗВАМЕ ПРОГРАМАТА?</h4>
-                <p>Това е програма работеща под операционната среда на Microsoft Windows XP/7/8/10 разработена за
-                    използване в Служба по Трудова Медицина. Програмата Ви дава възможност да извършвате регистриране на
-                    дейността си по контрол и консултации на фирмите относно дейността им свързана с трудовата
-                    медицина..</p>
-                <h4>II. ОПИСАНИЕ НА ФУНКЦИОНАЛНИТЕ ВЪЗМОЖНОСТИ НА ПРОГРАМАТА.</h4>
-                <p>Програмата се състои от две части, сървърна част и клиентска част.</p>
+
+        <div class="section_offset clearfix">
+            <h2>Програма за управление на дейността на Служба по трудова медицина</h2>
+			<div class="tour_section type_2">
+                <ul class="ts_nav clearfix">
+                    <li><a href="#tab-10">Къде да използваме</a></li>
+                    <li><a href="#tab-11">Описание</a></li>
+                    <li><a href="#tab-12">Инсталиране</a></li>
+                    <li><a href="#tab-13">Документация</a></li>
+                    <li><a href="#tab-14">Текуща версия</a></li>
+                    <li><a href="#tab-15">Съвместимост</a></li>
+                </ul>
+                <div class="ts_containers_wrap">
+                    <div id="tab-10" class="ts_container">
+                        <h4>I. Къде можем да използваме програмата?</h4>
+                        <p>Това е програма работеща под операционната среда на Microsoft Windows XP/7/8/10 разработена за
+                            използване в Служба по Трудова Медицина. Програмата Ви дава възможност да извършвате регистриране на
+                            дейността си по контрол и консултации на фирмите относно дейността им свързана с трудовата
+                            медицина..</p>
+                    </div>
+                    <div id="tab-11" class="ts_container">
+                        <h4>II. Описание на функционалните възможности на програмата.</h4>
+                        <p>Програмата се състои от две части, сървърна част и клиентска част.</p>
                 <p>Сървърна част от програмата – MaxtradeERP Server. Служи за управление, поддържане, архивиране и
                     описване на базата с данни. Програмата е изградена и компилирана на основата на език за програмиране
                     C#, в среда на Microsoft® .NET 3.5.</p>
@@ -104,8 +118,10 @@
                     риска<br />Коефициенти за вероятност за настъпване на вредата. С избор на менюто “Справки и Анализи”
                     &gt;&gt; “Оценка на риска”&gt;&gt;”Коефициенти за вероятност” се отваря прозорец в който ако желаете
                     можете да промените коефициентите и да запишете промените.</p>
-                <h4>III. ИНСТАЛИРАНЕ</h4>
-                <p>Продукта се състои от две части.</p>
+                    </div>
+                    <div id="tab-12" class="ts_container">
+                        <h4>III. Инсталиране</h4>
+                        <p>Продукта се състои от две части.</p>
                 <p>1. Сървърна, на която се съхранява базата от данни за всички извършени сделки. Инсталира се от файла
                     "MaxtradeCTMServer_Install.exe".</p>
                 <p>2. Клиентска част, с която Вие действително работите. Инсталира се от файла
@@ -114,14 +130,10 @@
                 <p>След коректната инсталация на сървърната и клиентската част на програмата можете да започнете работа
                     с нея. На поисканите данни за вход при първоначално стартиране на програмата можете да използвате
                     следните данни:<br />Потребител: admin<br />Парола: admin</p>
-                <h4>IV. ТЕКУЩА ВЕРСИЯ</h4>
-                <p>Текущата версия на MaxtradeCTM е: v.1.0.2.6 от 05.01.2016г.</p>
-                <h4>V. СЪВМЕСТИМОСТ</h4>
-                <p>Тествано за работоспособност на версии на<br />ОС Microsoft Windows: XP/7/8/8.1/10<br />ОС Microsoft
-                    Windows Server: 2003/2008/2012<br />Продукта е разработен и има необходимост от .NET 3.5</p>
-                <div style="background-color: #f3f3f3; padding: 4px 20px 10px 20px; border: thin solid #eeeeee;">
-                    <h4 style="margin-top: 15px; margin-bottom: 10px;">ДОКУМЕНТАЦИЯ</h4>
-                    <p><a class="btn btn-default" style="width: 100%; margin-top: 5px; margin-bottom: 5px;"
+                    </div>
+                    <div id="tab-13" class="ts_container">
+                        <h4>IV. Документация</h4>
+                        <p><a class="btn btn-default" style="width: 100%; margin-top: 5px; margin-bottom: 5px;"
                             href="/images/desktop/Documentation_CTM.pdf" target="_blank"
                             rel="noopener">Документация</a><br /><a class="btn btn-default"
                             style="width: 100%; margin-top: 5px; margin-bottom: 5px;"
@@ -139,37 +151,92 @@
                             style="width: 100%; margin-top: 5px; margin-bottom: 5px;"
                             href="https://www.youtube.com/watch?v=8dUsaDUNJkE" target="_blank" rel="noopener">Мултимедия
                             5</a></p>
-                </div>
-                <br />
-                <div style="background-color: #f3f3f3; padding: 4px 20px 10px 20px; border: thin solid #eeeeee;">
-                    <h4>ПРАВО НА ПОЛЗВАНЕ, ВАРИАНТИ, ЦЕНИ:</h4>
-                    <p><a class="btn btn-default" style="width: 100%;" href="/produkt/maxtrade-ctm/" rel="noopener">
-                            Maxtrade CTM<br />432.00 лв. с ДДС </a></p>
-                    <p class="small">* Лицензия за ползване на продукта на неограничен брой работни места за една
-                        година.</p>
-                    <p class="small" style="color: red;">Посочената цена е само за закупуване на самия модул. В цената
-                        не се включват инсталиране, настройване и промени в модула. тези допълнителни функции са въпрос
-                        на допълнителни разговори за определяне на цената спрямо конкретните изисквания на клиента.</p>
-                    <p class="small">Възможни са варианти за безплатно инсталиране и настройване на продукта.<br />Начин
-                        на закупуване и доставка:<br />След като направите заявка от нашия онлайн магазин</a><br />или директно на
-                        E-Mail: <span style="color: red;">home@avalonbg.com</span>, указвайки Вашите фирмени данни, ще
-                        Ви бъде изпратена проформа фактура за извършване на необходимото плащане за покупката. След
-                        удостоверяване на плащането, ще Ви бъде изпратена оригиналната фактура заедно с необходимите
-                        файлове за инсталацията на <span style="color: red;">Maxtrade CTM</span>.<br />При Ваше желание
-                        плащането може да се извърши и с наложен платеж чрез куриер.</p>
-                </div>
-                <br />
-                <div style="background-color: #f3f3f3; padding: 4px 20px 10px 20px; border: thin solid #eeeeee;">
-                    <h4>ИЗПОЛЗВАНЕ НА ВРЕМЕННА ВЕРСИЯ НА ПРОГРАМАТА:</h4>
-                    <p class="small">Можете да си изтеглите напълно работоспособна временна версия на програмата. Тя ще
-                        може да работи в продължение на един месец от инсталирането си. Всички функционалности на
-                        програмата са запазени. След изтичане на пробния период и при желание за закупуване на лиценз
-                        Вие няма да загубите натрупаните по време на теста данни.</p>
-                    <p><a href="https://avalonbg.com/download/MaxtradeCTM.zip">От тук можете да си изтеглите временна версия на Maxtrade CTM и Microsoft ReportViewer, необходим за печата на справките.</a></p>
+                    </div>
+                    <div id="tab-14" class="ts_container">
+                        <h4>V. Текуща версия</h4>
+                        <p>Текущата версия на MaxtradeCTM е: v.1.0.2.6 от 05.01.2016г.</p>
+                    </div>
+                    <div id="tab-15" class="ts_container">
+                        <h4>VI. Съвместимост</h4>
+                        <p>Тествано за работоспособност на версии на<br />ОС Microsoft Windows: XP/7/8/8.1/10<br />ОС Microsoft
+                            Windows Server: 2003/2008/2012<br />Продукта е разработен и има необходимост от .NET 3.5</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="section_offset clearfix">
+            <h3>Право на ползване, варианти, цени:</h3>
+            <div class="theme_box clearfix">
+                <p><a class="btn btn-default" style="width: 100%;" href="/produkt/maxtrade-ctm/" rel="noopener">
+                    Maxtrade CTM<br />432.00 лв. с ДДС </a></p>
+            <p class="small">* Лицензия за ползване на продукта на неограничен брой работни места за една
+                година.</p>
+            <p class="small" style="color: red;">Посочената цена е само за закупуване на самия модул. В цената
+                не се включват инсталиране, настройване и промени в модула. тези допълнителни функции са въпрос
+                на допълнителни разговори за определяне на цената спрямо конкретните изисквания на клиента.</p>
+            <p class="small">Възможни са варианти за безплатно инсталиране и настройване на продукта.<br />Начин
+                на закупуване и доставка:<br />След като направите заявка от нашия онлайн магазин</a><br />или директно на
+                E-Mail: <span style="color: red;">home@avalonbg.com</span>, указвайки Вашите фирмени данни, ще
+                Ви бъде изпратена проформа фактура за извършване на необходимото плащане за покупката. След
+                удостоверяване на плащането, ще Ви бъде изпратена оригиналната фактура заедно с необходимите
+                файлове за инсталацията на <span style="color: red;">Maxtrade CTM</span>.<br />При Ваше желание
+                плащането може да се извърши и с наложен платеж чрез куриер.</p>
+            </div>
+        </div>
+        <div class="call_to_action">
+            <div class="on_the_sides">
+                <div class="left_side">
+                    <div class="title">Maxtrade CTM</div>
+                    <p>Програма за управление на дейността на Служба по трудова медицина</p>
+                </div>
+                <div class="right_side">
+                    <a href="{{ route('product', ['id' => 7]) }}" class="button_blue huge_btn"><i class="icon-basket-1"></i>Купи сега!</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="section_offset clearfix">
+        </div>
+
+        <div class="section_offset clearfix">
+            <h4>Използване на временна версия на програмата:</h4>
+            <div class="theme_box clearfix">
+                <p class="small">Можете да си изтеглите напълно работоспособна временна версия на програмата. Тя ще
+                може да работи в продължение на един месец от инсталирането си. Всички функционалности на
+                програмата са запазени. След изтичане на пробния период и при желание за закупуване на лиценз
+                Вие няма да загубите натрупаните по време на теста данни.</p>
+            </div>
+        </div>
+        <div class="call_to_action type_2 on_the_sides">
+            <div class="left_side">
+                <div class="title">Maxtrade CTM</div>
+                <p>Програма за управление на дейността на Служба по трудова медицина</p>
+            </div>
+            <div class="right_side">
+                <a href="https://avalonbg.com/download/MaxtradeCTM.zip" onclick="clickBtnDownload(event)" class="button_blue huge_btn">({{ Product::where(['id' => 7])->first()->downloads }})&nbsp;Изтегли демо версия!</a>
+            </div>
+        </div>
+
     </div>
 </div>
 
+@endsection
+
+@section('scripts')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    function clickBtnDownload(e){
+        $.ajax({
+            type:'POST',
+            url:'/add-download.html',
+            data:{id:7}
+        });
+    };
+</script>
 @endsection
--- resources/views/desktop/maxtrade_smdc.blade.php
+++ resources/views/desktop/maxtrade_smdc.blade.php
@@ -1,15 +1,28 @@
+<?php use App\Product; ?>
 @extends('layouts.design')
 @section('content')
 
 <div class="page_wrapper">
     <div class="container">
-        <div class="section_offset">
-            <div class="row">
-                    <h2>Maxtrade SMDC Програма за управление на документите от Система по качеството</h2>
-                    <h4>I. КЪДЕ МОЖЕМ ДА ИЗПОЛЗВАМЕ ПРОГРАМАТА?</h4>
-                    <p>Maxtrade SMDC е програма работеща под операционната система на Microsoft Windows XP/7/8/10 разработена за използване в фирми използващи системи за качество. Програмата Ви дава възможност да извършвате регистриране на дейността си по контрола на документооборота в една фирма.</p>
-                    <h4>II. ОПИСАНИЕ НА ФУНКЦИОНАЛНИТЕ ВЪЗМОЖНОСТИ НА ПРОГРАМАТА.</h4>
-                    <p>Програмата се състои от две части, сървърна част и клиентска част.</p>
+
+        <div class="section_offset clearfix">
+            <h2>Maxtrade SMDC Програма за управление на документите от Система по качеството</h2>
+			<div class="tour_section type_2">
+                <ul class="ts_nav clearfix">
+                    <li><a href="#tab-10">Къде да използваме</a></li>
+                    <li><a href="#tab-11">Описание</a></li>
+                    <li><a href="#tab-12">Инсталиране</a></li>
+                    <li><a href="#tab-13">Текуща версия</a></li>
+                    <li><a href="#tab-14">Съвместимост</a></li>
+                </ul>
+                <div class="ts_containers_wrap">
+                    <div id="tab-10" class="ts_container">
+                        <h4>I. Къде можем да използваме програмата?</h4>
+                        <p>Maxtrade SMDC е програма работеща под операционната система на Microsoft Windows XP/7/8/10 разработена за използване в фирми използващи системи за качество. Програмата Ви дава възможност да извършвате регистриране на дейността си по контрола на документооборота в една фирма.</p>
+                    </div>
+                    <div id="tab-11" class="ts_container">
+                        <h4>II. Описание на функционалните възможности на програмата.</h4>
+                        <p>Програмата се състои от две части, сървърна част и клиентска част.</p>
                     <p>Сървърна част от програмата – MaxtradeSMDC Server. Служи за управление, поддържане, архивиране и описване на базата с данни. Програмата е изградена и компилирана на основата на език за програмиране C#, в среда на Microsoft® .NET 3.5.</p>
                     <h5>За нормалната работа на сървъра е необходимо следното програмно осигуряване.</h5>
                     <ul>
@@ -25,32 +38,89 @@
                     <li>Microsoft Chart Controls for Microsoft .NET Framework 3.5 Setup</li>
                     <li>Microsoft Report Viewer Redistributable 2008 Setup</li>
                     </ul>
-                    <h4>III. ИНСТАЛИРАНЕ</h4>
-                    <p>Продукта се състои от две части.</p>
+                    </div>
+                    <div id="tab-12" class="ts_container">
+                        <h4>III. Инсталиране</h4>
+                        <p>Продукта се състои от две части.</p>
                     <p>1. Сървърна, на която се съхранява базата от данни за всички извършени сделки. Инсталира се от файла "MaxtradeSMDCServer_Install.exe".</p>
                     <p>2. Клиентска част, с която Вие действително работите. Инсталира се от файла "MaxtradeSMDC_Instalator.exe".</p>
                     <p>3. Допълнения. Microsoft ReportViewer.exe. Необходим за печата на справките.</p>
                     <p>След коректната инсталация на сървърната и клиентската част на програмата можете да започнете работа</p>
-                    <h4>IV. ТЕКУЩА ВЕРСИЯ</h4>
-                    <p>Текущата версия на MaxtradeSMDC е: v.1.1.0.2 от 05.01.2016г.</p>
-                    <h4>V. СЪВМЕСТИМОСТ</h4>
-                    <p>Тествано за работоспособност на версии на<br />ОС Microsoft Windows: XP/7/8/8.1/10<br />ОС Microsoft Windows Server: 2003/2008/2012<br />Продукта е разработен и има необходимост от .NET 3.5</p>
-                    <div style="background-color: #f3f3f3; padding: 4px 20px 10px 20px; border: thin solid #eeeeee; margin-bottom: 10px;">
-                    <h4>ПРАВО НА ПОЛЗВАНЕ, ВАРИАНТИ, ЦЕНИ:</h4>
-                    <p><a class="btn btn-primary" style="width: 100%;" href="/produkt/maxtrade-smdc/" rel="noopener"> Maxtrade SMDC<br />99.00 лв. с ДДС </a></p>
+                    </div>
+                    <div id="tab-13" class="ts_container">
+                        <h4>IV. Текуща версия</h4>
+                        <p>Текущата версия на MaxtradeSMDC е: v.1.1.0.2 от 05.01.2016г.</p>
+                    </div>
+                    <div id="tab-14" class="ts_container">
+                        <h4>V. Съвместимост</h4>
+                        <p>Тествано за работоспособност на версии на<br />ОС Microsoft Windows: XP/7/8/8.1/10<br />ОС Microsoft Windows Server: 2003/2008/2012<br />Продукта е разработен и има необходимост от .NET 3.5</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section_offset clearfix">
+            <h3>Право на ползване, варианти, цени:</h3>
+            <div class="theme_box clearfix">
+                <p><a class="btn btn-primary" style="width: 100%;" href="/produkt/maxtrade-smdc/" rel="noopener"> Maxtrade SMDC<br />99.00 лв. с ДДС </a></p>
                     <p class="small">* Лицензия за ползване на продукта на едно работно място за една година.</p>
                     <p class="small" style="color: red;">Посочената цена е само за закупуване на самия модул. В цената не се включват инсталиране, настройване и промени в модула. тези допълнителни функции са въпрос на допълнителни разговори за определяне на цената спрямо конкретните изисквания на клиента.</p>
                     <p class="small">Възможни са варианти за безплатно инсталиране и настройване на продукта.<br />Начин на закупуване и доставка:<br />След като направите заявка от нашия онлайн магазин<br />или директно на E-Mail: <span style="color: red;">home@avalonbg.com</span>, указвайки Вашите фирмени данни, ще Ви бъде изпратена проформа фактура за извършване на необходимото плащане за покупката. След удостоверяване на плащането, ще Ви бъде изпратена оригиналната фактура заедно с необходимите файлове за инсталацията на <span style="color: red;">Maxtrade SMDC</span>.<br />При Ваше желание плащането може да се извърши и с наложен платеж чрез куриер.</p>
-                    </div>
-                    <p> </p>
-                    <div style="background-color: #f3f3f3; padding: 4px 20px 10px 20px; border: thin solid #eeeeee;">
-                    <h4>ИЗПОЛЗВАНЕ НА ВРЕМЕННА ВЕРСИЯ НА ПРОГРАМАТА:</h4>
-                    <p class="small">Можете да си изтеглите напълно работоспособна временна версия на програмата. Тя ще може да работи в продължение на един месец от инсталирането си. Всички функционалности на програмата са запазени. След изтичане на пробния период и при желание за закупуване на лиценз Вие няма да загубите натрупаните по време на теста данни.</p>
-                    <p><a href="https://avalonbg.com/download/MaxtradeSMDC.zip">От тук можете да си изтеглите временна версия на Maxtrade SMDC и Microsoft ReportViewer, необходим за печата на справките.</a></p>
-                    </div>
             </div>
         </div>
+        <div class="call_to_action">
+            <div class="on_the_sides">
+                <div class="left_side">
+                    <div class="title">Maxtrade SMDC</div>
+                    <p>Програма за управление на документите от Система по качеството</p>
+                </div>
+                <div class="right_side">
+                    <a href="{{ route('product', ['id' => 9]) }}" class="button_blue huge_btn"><i class="icon-basket-1"></i>Купи сега!</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="section_offset clearfix">
+        </div>
+
+        <div class="section_offset clearfix">
+            <h4>Използване на временна версия на програмата:</h4>
+            <div class="theme_box clearfix">
+                <p class="small">Можете да си изтеглите напълно работоспособна временна версия на програмата. Тя ще
+                може да работи в продължение на един месец от инсталирането си. Всички функционалности на
+                програмата са запазени. След изтичане на пробния период и при желание за закупуване на лиценз
+                Вие няма да загубите натрупаните по време на теста данни.</p>
+            </div>
+        </div>
+        <div class="call_to_action type_2 on_the_sides">
+            <div class="left_side">
+                <div class="title">Maxtrade SMDC</div>
+                <p>Програма за управление на документите от Система по качеството</p>
+            </div>
+            <div class="right_side">
+                <a href="https://avalonbg.com/download/MaxtradeSMDC.zip" onclick="clickBtnDownload(event)" class="button_blue huge_btn">({{ Product::where(['id' => 9])->first()->downloads }})&nbsp;Изтегли демо версия!</a>
+            </div>
+        </div>
+
     </div>
 </div>
 
+@endsection
+
+@section('scripts')
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    function clickBtnDownload(e){
+        $.ajax({
+            type:'POST',
+            url:'/add-download.html',
+            data:{id:9}
+        });
+    };
+</script>
 @endsection
