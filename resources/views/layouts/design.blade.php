c1da672ba66867a92e5f731bb2170d002ef7b761
Ilko Ivanov <ilko.iv@gmail.com>
2019-08-21 14:21:30 +0300

    addsence

--- resources/views/layouts/design.blade.php
+++ resources/views/layouts/design.blade.php
@@ -2,6 +2,14 @@
 <html lang="bg">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118342723-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-118342723-1');
+    </script>
     <!-- Basic page needs
 	============================================ -->
     <meta charset="utf-8">
