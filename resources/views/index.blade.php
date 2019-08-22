fc2dadd0539bab88cca4ffee53cd533a99487ce1
Ilko Ivanov <ilko.iv@gmail.com>
2019-08-22 16:07:55 +0300

    tbi index reklama

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -619,6 +619,134 @@
 
 			<!-- - - - - - - - - - - - - - End Page Wrapper - - - - - - - - - - - - - - - - -->
 
+            <!-- За вмъкване в началната страница на магазина -->
+		    @php
+            /* ######################### Зареждане на параметри ######################## */
+            $credittbi_unicid = '9ac3191c-d67c-47f3-aed0-135cc2d1f9cf';// Уникален идентификатор на магазина, получен от TBI Контролния Панел
+            /* ######################### Зареждане на параметри ######################## */
+            @endphp
+            <style>
+                *{padding:0;margin:0;}
+
+                /* tbi float */
+                .tbi-label-container{
+                    z-index:999;
+                    position:fixed;
+                    top:calc(100% / 2 - 130px);
+                    left:67px;
+                    display:table;
+                    visibility: hidden;
+                }
+                .tbi-label-text{
+                    width:410px;
+                    height:260px;
+                    color:#696969;
+                    background:#f5f5f5;
+                    display:table-cell;
+                    vertical-align:top;
+                    padding-left:5px;
+                    border:1px solid #f18900;
+                    border-radius:3px;
+                }
+                .tbi-label-text-a{
+                    text-align:center;
+                }
+                .tbi-label-text-a a{
+                    color:#b73607;
+                }
+                .tbi-label-text-a a:hover{
+                    color:#672207;
+                    text-decoration:underline;
+                }
+                .tbi-label-arrow{
+                    display:table-cell;
+                    vertical-align:middle;
+                    color:#f5f5f5;
+                    opacity:1;
+                }
+                .tbi_float{
+                    z-index:999;
+                    position:fixed;
+                    width:60px;
+                    height:60px;
+                    top:calc(100% / 2 - 30px);
+                    left:0px;
+                    background-color:#ffffff;
+                    border-top:1px solid #f18900;
+                    border-right:1px solid #f18900;
+                    border-bottom:1px solid #f18900;
+                    color:#FFF;
+                    border-top-right-radius:8px;
+                    border-bottom-right-radius:8px;
+                    text-align:center;
+                    box-shadow: 2px 2px 3px #999;
+                    cursor:pointer;
+                }
+                .tbi-my-float{
+                    margin-top:12px;
+                }
+            </style>
+            @php
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, 'https://api.tbibank.support/function/getparameters.php?cid='.$credittbi_unicid);
+            $paramstbi=json_decode(curl_exec($ch), true);
+            curl_close($ch);
+
+            //detect mobile
+            $useragent=$_SERVER['HTTP_USER_AGENT'];
+            if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))){
+                $deviceis = "mobile";
+            }else{
+                $deviceis = "pc";
+            }
+
+            //test 0%
+            if ($paramstbi['tbi_purcent_default'] == 2 || $paramstbi['tbi_purcent_default'] == 3 || $paramstbi['tbi_purcent_default'] == 4){
+                $tbi_picture = 'https://api.tbibank.support/calculators/assets/img/tbim10.png';
+            }else{
+                $tbi_picture = 'https://api.tbibank.support/calculators/assets/img/tbim' . $paramstbi['tbi_container_reklama'] . '.png';
+            }
+            @endphp
+
+            @if ($paramstbi['tbi_container_status'] == 'Yes')
+                @if ($deviceis == 'pc')
+                    <div class="tbi_float" onclick="tbiChangeContainer();">
+                        <img src="https://tbibank.support/dist/img/tbi_logo.png" class="tbi-my-float">
+                    </div>
+                @else
+                <div class="tbi_float" onclick="tbipaymentGoTo();">
+                    <img src="https://tbibank.support/dist/img/tbi_logo.png" class="tbi-my-float">
+                </div>
+                @endif
+                <div class="tbi-label-container">
+                    <div class="tbi-label-text">
+                        <div style="padding-bottom:5px;"></div>
+                        <img src="{{$tbi_picture}}">
+                        <div style="font-size:16px;padding-top:3px;">{{$paramstbi['tbi_container_txt1']}}</div>
+                        <p style="font-size:14px;">{{$paramstbi['tbi_container_txt2']}}</p>
+                        <div class="tbi-label-text-a"><a href="{{$paramstbi['tbi_backurl']}}" target="_blank" alt="ИНФОРМАЦИЯ ЗА ОНЛАЙН ПАЗАРУВАНЕ НА КРЕДИТ!">ИНФОРМАЦИЯ ЗА ОНЛАЙН ПАЗАРУВАНЕ НА КРЕДИТ!</a></div>
+                    </div>
+                </div>
+               <script type="application/javascript">
+                    function tbiChangeContainer(){
+                        var tbi_label_container = document.getElementsByClassName("tbi-label-container")[0];
+                        if (tbi_label_container.style.visibility == 'visible'){
+                            tbi_label_container.style.visibility = 'hidden';
+                            tbi_label_container.style.opacity = 0;
+                            tbi_label_container.style.transition = 'visibility 0s, opacity 0.5s ease';
+                        }else{
+                            tbi_label_container.style.visibility = 'visible';
+                            tbi_label_container.style.opacity = 1;
+                        }
+                    }
+                    function tbipaymentGoTo(url){
+                        window.open('{{$paramstbi['tbi_backurl']}}','_blank');
+                    }
+                </script>
+            @endif
+    <!-- За вмъкване в началната страница на магазина -->
 @endsection
 
 @section('scripts')
