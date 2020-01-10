5d9c01bc3f4bbef0564b48206f4d945b44692fd6
wiley68 <ilko.iv@gmail.com>
2020-01-10 14:25:53 +0200

    sitemap

--- resources/views/sitemap/categories.blade.php
+++ resources/views/sitemap/categories.blade.php
@@ -1,15 +1,11 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($categories as $category)
-        @php
-        $category_ids = [];
-        $category_ids[] = $category->id;
-        @endphp
-        <url>
-            <loc>{{ route('products', ['category_id' => $category_ids]) }}</loc>
-            <lastmod>{{ $last_update->tz('UTC')->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.6</priority>
-        </url>
+    <url>
+        <loc>{{ route('products', ['category_id' => $category->id]) }}</loc>
+        <lastmod>{{ $last_update->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.6</priority>
+    </url>
     @endforeach
 </urlset>
