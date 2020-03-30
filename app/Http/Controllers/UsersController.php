5b1fd499f1a436dc25ffeee03f21a9f64c442403
wiley68 <ilko.iv@gmail.com>
2020-03-30 14:10:09 +0300

    mail subscribe

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,9 +12,18 @@ use App\Project;
 use App\Support;
 use App\Favorite;
 use App\Review;
+use App\Product;
+use App\Software;
+use App\Manufacturer;
+use App\Property;
 
 class UsersController extends Controller
 {
+    public function getBaseUrl(){
+        $hostName = $_SERVER['HTTP_HOST']; 
+        return 'https://'.$hostName;
+    }
+
     public function loginRegisterUsers(){
         $root_categories = Category::where(['parent_id' => 0])->get();
 
@@ -311,6 +320,7 @@ class UsersController extends Controller
                 $user->save();
                 $root_categories = Category::where(['parent_id' => 0])->get();
 
+                $root_categories = Category::where(['parent_id' => 0])->get();
                 $webprojects = Project::all();
                 $supports_gamings = Support::where(['category_id' => 'gamings'])->get();
                 $supports_offices = Support::where(['category_id' => 'offices'])->get();
@@ -319,6 +329,116 @@ class UsersController extends Controller
                 $supports_networks = Support::where(['category_id' => 'networks'])->get();
                 $supports_cameras = Support::where(['category_id' => 'cameras'])->get();
                 $supports_softwares = Support::where(['category_id' => 'softwares'])->get();
+                $manufacturers = Manufacturer::all();
+                $properties = Property::first();
+
+                $deviceis = "";
+                $tbi_container_status = "";
+                $tbi_picture = "";
+                $tbi_container_txt1 = "";
+                $tbi_container_txt2 = "";
+                $tbi_backurl = "";
+                $uni_logo = "";
+                $uni_picture = "";
+                $uni_container_txt1 = "";
+                $uni_container_txt2 = "";
+                $uni_backurl = "";
+                $tbiyes = $properties->tbibank;
+                $uniyes = $properties->unicredit;
+
+                $useragent = $_SERVER['HTTP_USER_AGENT'];
+                if (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
+                    $deviceis = "mobile";
+                } else {
+                    $deviceis = "pc";
+                }
+
+                /** Credit */
+                /** TBI Bank */
+                if ($tbiyes) {
+                    $tbi_data = array(
+                        'name' => 'getAdvertising',
+                        'param' => array(
+                            'unicid' => '1af4d77b-70a2-4771-a04b-1c6d63f07680'
+                        )
+                    );
+                    // Encript data
+                    $tbi_plaintext = json_encode($tbi_data);
+                    $tbi_publicKey = openssl_pkey_get_public(file_get_contents('keys/pub.pem'));
+                    $tbi_a_key = openssl_pkey_get_details($tbi_publicKey);
+                    $tbi_chunkSize = ceil($tbi_a_key['bits'] / 8) - 11;
+                    $tbi_output = '';
+                    while ($tbi_plaintext) {
+                        $tbi_chunk = substr($tbi_plaintext, 0, $tbi_chunkSize);
+                        $tbi_plaintext = substr($tbi_plaintext, $tbi_chunkSize);
+                        $tbi_encrypted = '';
+                        if (!openssl_public_encrypt($tbi_chunk, $tbi_encrypted, $tbi_publicKey)) {
+                            die('Failed to encrypt data');
+                        }
+                        $tbi_output .= $tbi_encrypted;
+                    }
+                    openssl_free_key($tbi_publicKey);
+                    $tbi_output64 = base64_encode($tbi_output);
+                    // Encript data
+                    $tbi_ch = curl_init();
+                    curl_setopt_array($tbi_ch, array(
+                        CURLOPT_URL => "https://tbibank.support/api/index.php",
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 2,
+                        CURLOPT_TIMEOUT => 5,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "POST",
+                        CURLOPT_POSTFIELDS =>     json_encode(array('data' => $tbi_output64)),
+                        CURLOPT_HTTPHEADER => array(
+                            "Content-Type: application/json",
+                            "cache-control: no-cache"
+                        ),
+                    ));
+                    $responseapi = curl_exec($tbi_ch);
+                    $err = curl_error($tbi_ch);
+                    curl_close($tbi_ch);
+
+                    $api_obj = json_decode($responseapi);
+
+                    $tbi_container_status = $api_obj->data->result->advertising;
+                    $tbi_picture = $api_obj->data->result->picture;
+                    $tbi_container_txt1 = $api_obj->data->result->advertising_title;
+                    $tbi_container_txt2 = $api_obj->data->result->advertising_text;
+                    $tbi_backurl = $api_obj->data->result->url;
+                }
+
+                /** UNI Credit */
+                if ($uniyes) {
+                    $credituni_unicid = '9ce5287c-c8d1-4a22-878c-1a9d42d7160a';
+
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_URL, 'https://unicreditconsumerfinancing.info/function/getparameters.php?cid=' . $credituni_unicid);
+                    $paramsuni = json_decode(curl_exec($ch), true);
+                    curl_close($ch);
+
+                    $uni_logo = 'https://unicreditconsumerfinancing.info/dist/img/uni_logo.jpg';
+                    $uni_backurl = $paramsuni['uni_backurl'];
+                    $uni_picture = 'https://unicreditconsumerfinancing.info/calculators/assets/img/unim' . $paramsuni['uni_container_reklama'] . '.png';
+                    $uni_container_txt1 = $paramsuni['uni_container_txt1'];
+                    $uni_container_txt2 = $paramsuni['uni_container_txt2'];
+                    $uni_container_status = $paramsuni['uni_container_status'];
+                }
+
+                $software_desktop = Software::where(['category_id' => 'desktop'])->get();
+                $software_modules = Software::where(['category_id' => 'modules'])->get();
+                $software_websoftware = Software::where(['category_id' => 'websoftware'])->get();
+                $software_webservice = Software::where(['category_id' => 'webservice'])->get();
+                $software_mobile = Software::where(['category_id' => 'mobile'])->get();
+                $software_industry = Software::where(['category_id' => 'industry'])->get();
+                /** Credit */
+
+                $product1 = Product::where('instock', '!=', 'няма наличност')->orderByRaw("RAND()")->first();
+                $product2 = Product::where('instock', '!=', 'няма наличност')->orderByRaw("RAND()")->first();
+                $product3 = Product::where('instock', '!=', 'няма наличност')->orderByRaw("RAND()")->first();
+
                 return view('index')->with([
                     'title' => 'Софтуер - продажба на компютърна техника | Авалон',
                     'description' => 'Проектиране и инсталиране на софтуер. Продажба на компютърна техника.',
@@ -332,7 +452,32 @@ class UsersController extends Controller
                     'supports_networks' => $supports_networks,
                     'supports_cameras' => $supports_cameras,
                     'supports_softwares' => $supports_softwares,
-                    'message' => 'Успешно се записахте за получаване на новини!'
+                    'message' => 'Успешно се записахте за получаване на новини!',
+                    'product1' => $product1,
+                    'product2' => $product2,
+                    'product3' => $product3,
+                    'baseurl' => $this->getBaseUrl(),
+                    'software_desktop' => $software_desktop,
+                    'software_modules' => $software_modules,
+                    'software_websoftware' => $software_websoftware,
+                    'software_webservice' => $software_webservice,
+                    'software_mobile' => $software_mobile,
+                    'software_industry' => $software_industry,
+                    'manufacturers' => $manufacturers,
+                    'tbiyes' => $tbiyes,
+                    'tbi_container_status' => $tbi_container_status,
+                    'tbi_picture' => $tbi_picture,
+                    'tbi_container_txt1' => $tbi_container_txt1,
+                    'tbi_container_txt2' => $tbi_container_txt2,
+                    'tbi_backurl' => $tbi_backurl,
+                    'uni_container_status' => $uni_container_status,
+                    'uni_logo' => $uni_logo,
+                    'uni_picture' => $uni_picture,
+                    'uni_container_txt1' => $uni_container_txt1,
+                    'uni_container_txt2' => $uni_container_txt2,
+                    'uni_backurl' => $uni_backurl,
+                    'deviceis' => $deviceis,
+                    'uniyes' => $uniyes
                 ]);
             }else{
                 return redirect('/')->withErrors(['Грешка при запис за новини:', 'Необходимо е да имате създаден профил в магазина за да използвате услугата!']);
