db375641dfa62fb976133f36c5e42afcb4b17a61
boriananikolova <boriana.nikolova7@gmail.com>
2020-09-03 15:20:16 +0300

    google changes

--- config/services.php
+++ config/services.php
@@ -49,7 +49,7 @@ return [
     'google' => [    
         'client_id' => env('GOOGLE_CLIENT_ID'),  
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),  
-        'redirect' => env('FACEBOOK_REDIRECT_URI') 
+        'redirect' => env('GOOGLE_REDIRECT_URI') 
     ],
 
 ];
